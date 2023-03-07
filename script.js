d4def5d64fc7feeffedc4e384985bf43c8d87ac4
busjee <busakbalazs@gmail.com>
2023-03-07 16:45:42 +0000

    function to create cards in html

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
+let cardContainer = document.querySelector(".card-container");
 let myLibrary = [];
 
-function Book(title, author, pageNum, read) {
-    this.title = title;
-    this.author = author;
-    this.pageNum = pageNum;
-    this.read = read;
-    this.info = function() {
-        console.log(title, 'by', author, pageNum, 'pages', read)
-    };
-};
+function createCards() {
+  cardContainer.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let card = document.createElement("div");
 
-// const TheHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
-// TheHobbit.info();
+    card.className = "card";
+
+    card.innerHTML = "<h3>" + myLibrary[i].title + "</h3><p>" + myLibrary[i].author + "</p><p>" + myLibrary[i].pageNum + "</p><p>" + myLibrary[i].read + "</p>";
+
+    cardContainer.appendChild(card);
+  }
+}
 
 function addBookToLibrary() {
-    // get user input values from HTML form
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let pageNum = document.getElementById("page-number").value;
-    let read = document.getElementById("read").value;
+  // get user input values from HTML form
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pageNum = document.getElementById("page-number").value;
+  let read = document.getElementById("read").value;
 
-    let newBook = {
-        title: title,
-        author: author,
-        pageNum: pageNum,
-        read: read
-    };
+  let newBook = {
+    title: title,
+    author: author,
+    pageNum: pageNum,
+    read: read
+  };
 
-    myLibrary.push(newBook);
-    console.log(myLibrary);
+  myLibrary.push(newBook);
+
+  createCards();
 };
 
-
-
-
+// render initial cards
+createCards();
